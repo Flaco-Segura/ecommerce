c691e5cac59f93b030de7060c3c7957beff0be0d
Scarnouse <manquesada@gmail.com>
2020-10-04 10:04:29 +0200

    cart service continue

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -37,6 +37,28 @@ export class CartService {
   }
 
   computeCartTotals() {
-    
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let cartItem of this.cartItems) {
+      totalPriceValue += cartItem.quantity * cartItem.unitPrice;
+      totalQuantityValue += cartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log('Contents of the cart');
+    for(let cartItem of this.cartItems) {
+      const subTotalPrice = cartItem.quantity * cartItem.unitPrice;
+      console.log(`name: ${cartItem.name}, quantity: ${cartItem.quantity}, unitPrice: ${cartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+    console.log('=== 000 ===');
   }
 }

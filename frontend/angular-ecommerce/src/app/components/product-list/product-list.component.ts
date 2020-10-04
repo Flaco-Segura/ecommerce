374c5790c8c3aaf18ce38108dc50feb945057c3f
Scarnouse <manquesada@gmail.com>
2020-10-04 10:09:09 +0200

    cart service injected

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -21,6 +23,7 @@ export class ProductListComponent implements OnInit {
   totalElements:number = 0;
   
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -94,6 +97,8 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(product: Product) {
-    
+    const cartItem = new CartItem(product);
+
+    this.cartService.addToCart(cartItem);
   }
 }

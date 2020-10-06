b036f7b782ca96848c7a84808739856f97a589b3
Scarnouse <manquesada@gmail.com>
2020-10-06 19:03:46 +0200

    upgrading style for remove button and checkout component

--- frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -40,4 +40,8 @@ export class CartDetailsComponent implements OnInit {
   decrementQuantity(cartItem: CartItem){
     this.cartService.decrementQuantity(cartItem);
   }
+
+  remove(cartItem: CartItem) {
+    this.cartService.remove(cartItem);
+  }
 }

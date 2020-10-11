6fb4751187cf42bbc586973caabbbe28da87ab63
Scarnouse <manquesada@gmail.com>
2020-10-11 07:09:46 +0200

    country and state repository and entity refactor

--- backend/spring-boot-ecommerce/src/main/java/com/flacosegura/ecommerce/entity/Country.java
+++ backend/spring-boot-ecommerce/src/main/java/com/flacosegura/ecommerce/entity/Country.java
@@ -2,21 +2,16 @@ package com.flacosegura.ecommerce.entity;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name="country")
-@Getter
-@Setter
+@Getter @Setter
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,5 +25,6 @@ public class Country {
     private String name;
 
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<State> states;
 }

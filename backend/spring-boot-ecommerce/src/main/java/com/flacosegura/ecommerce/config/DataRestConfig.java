48754d6500e7f3e7c28677d67dea6fe514e68373
Scarnouse <manquesada@gmail.com>
2020-10-11 07:31:00 +0200

    refactoring data rest configuration

--- backend/spring-boot-ecommerce/src/main/java/com/flacosegura/ecommerce/config/DataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/flacosegura/ecommerce/config/DataRestConfig.java
@@ -7,8 +7,10 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
 
+import com.flacosegura.ecommerce.entity.Country;
 import com.flacosegura.ecommerce.entity.Product;
 import com.flacosegura.ecommerce.entity.ProductCategory;
+import com.flacosegura.ecommerce.entity.State;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -31,18 +33,13 @@ public class DataRestConfig implements RepositoryRestConfigurer{
         // disable PUT, POST, and DELETE
         HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
-        // disable HTTP methods for Product
-        config.getExposureConfiguration()
-            .forDomainType(Product.class)
-            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
-            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        // disable HTTP methods for Product, ProductCategory, Country and State
+        disableHttpMethods(config, unsupportedActions, Product.class);
+        disableHttpMethods(config, unsupportedActions, ProductCategory.class);
+        disableHttpMethods(config, unsupportedActions, Country.class);
+        disableHttpMethods(config, unsupportedActions, State.class);
 
-        // disable HTTP methods for ProductCategory
-        config.getExposureConfiguration()
-            .forDomainType(ProductCategory.class)
-            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
-            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
-    
+        
         // call an internal helper method
         exposeIds(config);
 
@@ -59,5 +56,13 @@ public class DataRestConfig implements RepositoryRestConfigurer{
 
         Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private void disableHttpMethods(RepositoryRestConfiguration config, HttpMethod[] unsupportedActions, Class domainType) {
+        config.getExposureConfiguration()
+            .forDomainType(domainType)
+            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
     }
 }
